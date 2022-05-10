c36388a5a9e8199a84a90cbdeba7455437d77df5
Jobayer <mrahman192070@bscse.uiu.ac.bd>
2022-05-10 22:13:27 +0600

    implement Factory Pattern

--- src/com/company/Painter.java
+++ src/com/company/Painter.java
@@ -1,28 +1,72 @@
 package com.company;
 import java.util.Scanner;
 
-public class Painter {
+interface Shape
+{
+	void draw();
+}
 
-        public static void main(String[] args) throws Exception
-        {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter a string: ");
-            String type= sc.nextLine();
 
-            switch (type)
-            {
-                case "Circle":
-                  System.out.println("a circle drawn");
-                    break;
-                case "Square":
-                   System.out.println("a square drawn");
-                    break;
-                case "Rectangle":
-                    System.out.println("a rectangle drawn");
-                    break;
-                default:
-                    System.out.println( "Shape type : "+type+" cannot be instantiated");
+class Circle implements Shape
+{
+	 @Override
+	 public void draw()
+	 {
+		 System.out.println("a circle drawn");
+	 }
+}
 
-            }
-        }
-    }
+
+class Square implements Shape
+{
+	 @Override
+	 public void draw()
+	 {
+		 System.out.println("a square drawn");
+	 }
+}
+
+class Rectangle implements Shape
+{
+	 @Override
+	 public void draw()
+	 {
+		 System.out.println("a rectangle drawn");
+	 }
+}
+
+
+class ShapeFactory
+{
+ 
+	 public Shape getShape(String type) throws Exception
+	 {
+		 switch (type)
+		 {
+			 case "Circle":
+				 return new Circle();
+			 case "Square":
+				 return new Square();
+			 case "Rectangle":
+				 return new Rectangle();
+			 default:
+				 throw new Exception( "Shape type : "+type+" cannot be instantiated");
+		 }
+	 }
+}
+
+class Painter
+{
+	 public static void main(String[] args) throws Exception
+	 {
+		 
+		 ShapeFactory shapeFactory = new ShapeFactory();
+		 Shape circle=shapeFactory.getShape("Circle");
+		 circle.draw();
+		 Shape square = shapeFactory.getShape("Square");
+		 square.draw();
+		 
+		 Shape rombus = shapeFactory.getShape("Rombus");
+		 rombus.draw();
+	 }
+}
